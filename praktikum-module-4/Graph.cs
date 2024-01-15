7a77ddbcaa05b7d7c2aee9800b5fd9a58630c020
adenafil <nafilie9@gmail.com>
2024-01-15 14:12:23 +0700

    add Graph class

---
+++ praktikum-module-4/Graph.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: acer
+ * Date: 15/01/2024
+ * Time: 14:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace praktikum_module_4
+{
+	/// <summary>
+	/// Description of Graph.
+	/// </summary>
+	public class Graph
+	{
+		public Graph()
+		{
+		}
+	}
+}
