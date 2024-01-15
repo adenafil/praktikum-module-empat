cecee1400c838699b849254e6aa0a174a8e4bb7b
adenafil <nafilie9@gmail.com>
2024-01-15 14:08:14 +0700

    first commit

---
+++ praktikum-module-4/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: acer
+ * Date: 15/01/2024
+ * Time: 14:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace praktikum_module_4
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
